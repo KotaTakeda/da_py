629d5e4742d1e0c0b7c35461b37984e901e2ab12
KotaTakeda <takeda11.kl@outlook.jp>
2023-06-24 11:16:33 +0900

    update name

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     description='',
     long_description='',
     license='MIT',
-    package_dir={'da_py': 'src'},
+    package_dir={'da': 'src'},
     install_requires=['numpy>=1.19', 'scipy>=1.1'],  # not strict
 )
