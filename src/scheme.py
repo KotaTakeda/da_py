ad678cd536275841185a0759fd13113c9bb3853c
KotaTakeda <takeda11.kl@outlook.jp>
2024-01-12 12:58:26 +0900

    add scheme

--- src/scheme.py
+++ src/scheme.py
@@ -1,4 +1,7 @@
+from scipy.optimize import newton
 
+
+# 4th order Runge Kutta
 def rk4(f, t, y, dt):
     k1 = f(t, y)
     k2 = f(t + dt / 2, y + k1 * dt / 2)
@@ -12,3 +15,11 @@ def rk4(f, t, y, dt):
 def euler(f, t, x, dt):
     return x + f(t, x) * dt
 
+
+def implicit_midpoint(f, t, x, dt):
+    def implicit_eq(x_next):
+        return x + dt * f(t + dt/2, (x + x_next) / 2) - x_next
+
+    x_next_guess = x + dt * f(t + dt/2, x)
+    x_next = newton(implicit_eq, x_next_guess)
+    return x_next
