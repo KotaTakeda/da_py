448717a68aaf9c8453b362c8e650ab40760c8444
KotaTakeda <takeda11.kl@outlook.jp>
2024-05-10 13:15:04 +0900

    add plot xindex

--- src/visualize.py
+++ src/visualize.py
@@ -1,10 +1,14 @@
 import matplotlib.pyplot as plt
 
 
-def plot_loss(result1, result2, loss_func, ax=None, **kargs):
+def plot_loss(result1, result2, loss_func, x_index=None, ax=None, **kargs):
     assert len(result1) == len(result2)
     loss = loss_func(result1, result2)
     if ax is None:
         fig, ax = plt.subplots()
-    ax.plot(loss, **kargs)
+    if x_index is None:
+        ax.plot(loss, **kargs)
+    else:
+        assert len(x_index) == len(loss)
+        ax.plot(x_index, loss, **kargs)
     return loss
