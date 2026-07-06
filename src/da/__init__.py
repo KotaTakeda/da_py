ecd217fce5b05f457c7bec9bd1fafdceea61f869
KotaTakeda <takeda11.kl@outlook.jp>
2026-07-06 23:43:22 +0900

    Define versioning policy and bump to 0.7.0

--- src/da/__init__.py
+++ src/da/__init__.py
@@ -1,7 +1,12 @@
+from importlib.metadata import PackageNotFoundError, version
+
 from da.enkfn import EnKFN, estimate_l1_enkfn_dual
 from da.etkf import ETKF
 from da.exkf import ExKF
 
-__version__ = "0.6"
+try:
+    __version__ = version("da_py")
+except PackageNotFoundError:
+    __version__ = "0.0.0+unknown"
 
 __all__ = ["ETKF", "ExKF", "EnKFN", "estimate_l1_enkfn_dual"]
